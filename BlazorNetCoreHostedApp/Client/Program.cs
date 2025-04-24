b297343200942e0e0a75e0e4913e12932875de06
DIGINESIS\aurelian <aurelian.stancu@diginesis.com>
2025-04-24 20:34:39 +0300

    upd Program cs in client project

--- BlazorNetCoreHostedApp/Client/Program.cs
+++ BlazorNetCoreHostedApp/Client/Program.cs
@@ -6,7 +6,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("BlazorNetCoreHostedApp.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+//builder.Services.AddHttpClient("BlazorNetCoreHostedApp.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+builder.Services.AddHttpClient("BlazorNetCoreHostedApp.ServerAPI", client =>
+{
+#if DEBUG
+	client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+#else
+    client.BaseAddress = new Uri("https://kind-rock-0b7318210.6.azurestaticapps.net/");
+#endif
+});
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorNetCoreHostedApp.ServerAPI"));
